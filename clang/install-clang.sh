632683344039857fef7f2d880427b74476a131c2
Björn Fahller <bjorn@fahller.se>
2022-11-13 12:37:24 +0100

    Fix clang-15 installation

--- clang/install-clang.sh
+++ clang/install-clang.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
-# Adapted from https://github.com/silkeh/docker-clang/blob/master/install.sh.
 set -eou pipefail
 
 VERSION=$1
 
 
 fetch_clang () {
+    source /etc/lsb-release
     apt install -y software-properties-common
     wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|apt-key add -
-    apt-add-repository "deb http://apt.llvm.org/unstable/ llvm-toolchain-$VERSION main"
-    apt-add-repository "deb-src http://apt.llvm.org/unstable/ llvm-toolchain-$VERSION main"
+    apt-add-repository "deb http://apt.llvm.org/${DISTRIB_CODENAME}/ llvm-toolchain-${DISTRIB_CODENAME}-$VERSION main"
+    apt-add-repository "deb-src http://apt.llvm.org/${DISTRIB_CODENAME}/ llvm-toolchain-${DISTRIB_CODENAME}-$VERSION main"
     apt update
     apt install -y clang-$VERSION libc++-$VERSION-dev libc++abi-${VERSION}-dev
 }
