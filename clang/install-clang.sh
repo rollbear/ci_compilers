95305656a8f4cc53dcdda625769e12604f8b2d21
Björn Fahller <bjorn@fahller.se>
2023-11-09 12:48:17 +0100

    Install libfuzzer for clang containers

--- clang/install-clang.sh
+++ clang/install-clang.sh
@@ -11,7 +11,7 @@ fetch_clang () {
     apt-add-repository "deb http://apt.llvm.org/${DISTRIB_CODENAME}/ llvm-toolchain-${DISTRIB_CODENAME}-$VERSION main"
     apt-add-repository "deb-src http://apt.llvm.org/${DISTRIB_CODENAME}/ llvm-toolchain-${DISTRIB_CODENAME}-$VERSION main"
     apt update
-    apt install -y clang-$VERSION libc++-$VERSION-dev libc++abi-${VERSION}-dev
+    apt install -y clang-$VERSION libc++-$VERSION-dev libc++abi-${VERSION}-dev libfuzzer-$VERSION-dev
 }
 
 apt install -y clang-${VERSION} || fetch_clang clang-${VERSION}
