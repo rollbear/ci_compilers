6ca76efd6cdbdc009479e027fc15c32a642315fb
Björn Fahller <bjorn@fahller.se>
2024-07-10 18:58:46 +0200

    Add new glibc last in link library path list

--- common/install-glibc.sh
+++ common/install-glibc.sh
@@ -11,21 +11,24 @@ if [ "$(greater_version $MIN_VERSION $CURRENT_VERSION)" == "${CURRENT_VERSION}" 
 then
   exit 0
 fi
-apt install -y --no-install-recommends gawk bison texinfo gcc
+apt install -y --no-install-recommends gawk bison texinfo g++
 git clone -b glibc-${MIN_VERSION} --depth=1 git://sourceware.org/git/glibc
 lscpu
 mkdir glibc/build
 pushd glibc/build
-../configure --disable-sanity-checks
-make -j 4
-make -j 4 install
-popd
-[ ! -e /usr/include/xlocale.h ] || { \
-  rm /usr/include/xlocale.h \
-  ln -s /usr/include/locale.h /usr/include/xlocale.h \
-}
-apt remove -y gawk bison texinfo gcc
-apt autoremove -y
-rm -rf glibc
+../configure --disable-sanity-checks  --prefix=/opt/glibc
 
-}
+make -j 4
+make  install
+popd
+if [ -e /usr/include/xlocale.h ]
+then
+  rm /usr/include/xlocale.h
+  ln -s /usr/include/locale.h /usr/include/xlocale.h
+fi
+
+echo /opt/glibc/lib >> /etc/ld.so.conf.d/x86_64-linux-gnu.conf
+
+rm -rf glibc
+apt remove -y gawk bison texinfo g++
+apt autoremove -y
