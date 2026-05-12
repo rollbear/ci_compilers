5a271e64e150f709032298afd374e6c0a7b09810
Björn Fahller <bjorn@fahller.se>
2026-05-12 11:05:43 +0200

    Keep benchmark v1.9.0 for pre-c++17 builds

--- common/install-benchmark.sh
+++ common/install-benchmark.sh
@@ -2,22 +2,33 @@
 set -eou pipefail
 
 readonly VERSION=v1.9.5
+readonly FALLBACK_VERSION=v1.9.0
 readonly CXX=$1
 readonly CXX_STANDARDS="$2"
 readonly CXXLIB=$3
 readonly CXXFLAGS=${CXXLIB:+"-stdlib=${CXXLIB}"}
 
-echo "Downloading benchmark ${VERSION}"
-wget -nv -O benchmark_src.tar.gz https://github.com/google/benchmark/archive/refs/tags/${VERSION}.tar.gz
-mkdir benchmark_src
-tar xf benchmark_src.tar.gz -C benchmark_src --strip-components=1
 
-for STD in ${CXX_STANDARDS}
-do
-    echo "Building benchmark ${VERSION} for C++${STD}${CXXLIB}"
+download_version () {
+    version=$1
+    echo "Downloading benchmark ${version}"
+    dir="benchmark_src-$version"
+    [ -d "$dir" ] || {
+        wget -nv -O benchmark_src.tar.gz https://github.com/google/benchmark/archive/refs/tags/${version}.tar.gz
+        mkdir "$dir"
+        tar xf benchmark_src.tar.gz -C "$dir" --strip-components=1
+    }
+}
+
+install_version () {
+    version=$1
+    download_version "$version"
+    echo "Building benchmark ${version} for C++${STD}${CXXLIB}"
     PREFIX="/usr/local/lib/c++${STD}${CXXLIB}/benchmark"
-    mkdir build
-    cmake -B build \
+    builddir="build-$version"
+    [ -e "$builddir" ] && rm -rf "$builddir"
+    mkdir "$builddir"
+    cmake -B "$builddir" \
           -DCMAKE_CXX_COMPILER=${CXX} \
           -DCMAKE_CXX_STANDARD=${STD} \
           -DCMAKE_INSTALL_PREFIX=${PREFIX} \
@@ -26,9 +37,14 @@ do
           -DHAVE_STD_REGEX=ON \
           -DBENCHMARK_ENABLE_TESTING=NO \
           -DCMAKE_VERBOSE_MAKEFILE=yes \
-          -S benchmark_src \
-          -G Ninja &&
-    cmake --build build && cmake --install build && rm -r build
+          -S "benchmark_src-$version" \
+          -G Ninja && \
+    cmake --build "$builddir" && cmake --install "$builddir" && rm -r "$builddir"
+}
+
+for STD in ${CXX_STANDARDS}
+do
+    install_version $VERSION || install_version $FALLBACK_VERSION
 done
 echo "Cleaning up"
-rm -r benchmark*
+rm -rf benchmark* build*
